96e5b7766478c580af847a23b440c0ac2dcd5095
Ruslan Bobrov <ruslan.bobrov.spb@gmail.com>
2022-11-26 13:32:04 +0300

    my commit_1

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-def main():
-    print('My first git program')
-    print('hello')
-    print('hello2')
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QButtonGroup
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+        con = sqlite3.connect('coffee.sqlite')
+
+        cur = con.cursor()
+
+        result = cur.execute("""SELECT * FROM coffe_data
+        """).fetchall()
+        con.close()
+        for elem in result:
+            res = ''
+            for i in elem:
+                res += str(i) + ' '
+            self.listWidget.addItem(res[:-1])
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
